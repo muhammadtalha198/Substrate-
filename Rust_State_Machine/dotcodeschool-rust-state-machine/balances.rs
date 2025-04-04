6c2000f63d5f45057c6451d529a9fa51c736b742
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-04-04 15:16:09 +0500

    Update balances.rs

--- Rust_State_Machine/dotcodeschool-rust-state-machine/balances.rs
+++ Rust_State_Machine/dotcodeschool-rust-state-machine/balances.rs
@@ -13,12 +13,33 @@ impl Pallet {
 		}
 	}
 
+    /// Set the balance of an account `who` to `amount`.    
     pub fn set_balance(&mut self, who: &str, amount: u128) {
         self.balances.insert(who.to_string(), amount);
     }
 
+    /// Get the balance of an account `who`.
+	/// If the account has no stored balance, we return zero.
     pub fn get_balance(&self, who: &str) -> Option<&u128> {
         self.balances.get(who).unwrap_or(&0)
+    }
+
+    
+    /// /// Transfer `amount` from one account to another.
+	/// This function verifies that `from` has at least `amount` balance to transfer,
+	/// and that no mathematical overflows occur.
+    pub fn transfer(&mut self, from: &str, to: &str, amount: u128) -> Result<(), String> {
+        let from_balance = self.get_balance(from).unwrap();
+        let to_balance = self.get_balance(to).unwrap();
+
+        if from_balance < amount {
+            return Err("Insufficient balance".to_string());
+        }
+
+        self.balances.insert(from.to_string(), from_balance - amount);
+        self.balances.insert(to.to_string(), to_balance + amount);
+
+        Ok(())
     }
 }
 
@@ -34,4 +55,15 @@ mod tests {
         assert_eq!(balances.balance(&"alice".to_string()), 100);
         assert_eq!(balances.balance(&"bob".to_string()), 0);
 	}
+
+    #[test]
+    fn transfer_balance(){
+        let mut balances = super::Pallet::new();
+
+        balances.set_balance(&"alice".to_string(), 100);
+        balances.set_balance(&"bob".to_string(), 0);
+
+        assert!(balances.transfer(&"alice".to_string(), &"bob".to_string(), 100).is_ok());  
+        assert_eq!(balances.balance(&"alice".to_string()), 0);
+    }
 }
