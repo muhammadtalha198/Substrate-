5c1981617e948cc64e6e826d6727d7d9e030ab05
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-04-07 19:58:55 +0500

    Update balances.rs

--- Rust_State_Machine/dotcodeschool-rust-state-machine/balances.rs
+++ Rust_State_Machine/dotcodeschool-rust-state-machine/balances.rs
@@ -1,45 +1,51 @@
+use num::traits::{CheckedAdd, CheckedSub, Zero};
 use std::collections::BTreeMap;
 
-/// This is the Balances Module.
-/// It is a simple module which keeps track of how much balance each account has in this state
-/// machine.
-/* TODO: Add the derive macro to implement the `Debug` trait for `Pallet`. */
-pub struct Pallet {
-	// A simple storage mapping from accounts (`String`) to their balances (`u128`).
-	balances: BTreeMap<String, u128>,
+
+
+pub trait Config: crate::system::Config {
+
+	type Balance: Zero + CheckedSub + CheckedAdd + Copy;
 }
 
-impl Pallet {
-	/// Create a new instance of the balances module.
+
+
+
+#[derive(Debug)]
+pub struct Pallet<T: Config> {
+	
+	balances: BTreeMap<T::AccountId, T::Balance>,
+}
+
+impl<T: Config> Pallet<T> {
+	
 	pub fn new() -> Self {
 		Self { balances: BTreeMap::new() }
 	}
 
-	/// Set the balance of an account `who` to some `amount`.
-	pub fn set_balance(&mut self, who: &String, amount: u128) {
+	
+	pub fn set_balance(&mut self, who: &T::AccountId, amount: T::Balance) {
 		self.balances.insert(who.clone(), amount);
 	}
 
-	/// Get the balance of an account `who`.
-	/// If the account has no stored balance, we return zero.
-	pub fn balance(&self, who: &String) -> u128 {
-		*self.balances.get(who).unwrap_or(&0)
+	
+	
+	pub fn balance(&self, who: &T::AccountId) -> T::Balance {
+		*self.balances.get(who).unwrap_or(&T::Balance::zero())
 	}
 
-	/// Transfer `amount` from one account to another.
-	/// This function verifies that `from` has at least `amount` balance to transfer,
-	/// and that no mathematical overflows occur.
+	
 	pub fn transfer(
 		&mut self,
-		caller: String,
-		to: String,
-		amount: u128,
-	) -> Result<(), &'static str> {
+		caller: T::AccountId,
+		to: T::AccountId,
+		amount: T::Balance,
+	) -> crate::support::DispatchResult {
 		let caller_balance = self.balance(&caller);
 		let to_balance = self.balance(&to);
 
-		let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Not enough funds.")?;
-		let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow")?;
+		let new_caller_balance = caller_balance.checked_sub(&amount).ok_or("Not enough funds.")?;
+		let new_to_balance = to_balance.checked_add(&amount).ok_or("Overflow")?;
 
 		self.balances.insert(caller, new_caller_balance);
 		self.balances.insert(to, new_to_balance);
@@ -50,9 +56,21 @@ impl Pallet {
 
 #[cfg(test)]
 mod tests {
+	struct TestConfig;
+
+	impl crate::system::Config for TestConfig {
+		type AccountId = String;
+		type BlockNumber = u32;
+		type Nonce = u32;
+	}
+
+	impl super::Config for TestConfig {
+		type Balance = u128;
+	}
+
 	#[test]
 	fn init_balances() {
-		let mut balances = super::Pallet::new();
+		let mut balances = super::Pallet::<TestConfig>::new();
 
 		assert_eq!(balances.balance(&"alice".to_string()), 0);
 		balances.set_balance(&"alice".to_string(), 100);
@@ -62,7 +80,7 @@ mod tests {
 
 	#[test]
 	fn transfer_balance() {
-		let mut balances = super::Pallet::new();
+		let mut balances = super::Pallet::<TestConfig>::new();
 
 		assert_eq!(
 			balances.transfer("alice".to_string(), "bob".to_string(), 51),
